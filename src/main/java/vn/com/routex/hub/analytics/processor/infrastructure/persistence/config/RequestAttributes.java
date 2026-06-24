5afa0fc7d07bd0861886f5224d37984a5cc1a5a1
Bao Pham <louis.phammm@gmail.com>
2026-06-24 18:17:33 +0700

    configuration for cors api integration

--- src/main/java/vn/com/routex/hub/analytics/processor/infrastructure/persistence/config/RequestAttributes.java
+++ src/main/java/vn/com/routex/hub/analytics/processor/infrastructure/persistence/config/RequestAttributes.java
@@ -2,7 +2,7 @@ package vn.com.routex.hub.analytics.processor.infrastructure.persistence.config;
 
 public interface RequestAttributes {
     String REQUEST_ID = "RT-REQUEST-ID";
-    String REQUEST_DATE_TIME = "RT-REQUEST_DATE_TIME";
+    String REQUEST_DATE_TIME = "RT-REQUEST-DATE-TIME";
     String CHANNEL = "RT-CHANNEL";
     String MERCHANT_ID = "merchantId";
     String USER_EMAIL = "userEmail";
